b4b35c0fbf4d0ed3c5f6458ef4112da40c7b7193
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-02 21:57:53 +0200

    [SQUASH] config: Enable inline sigining for user and userdebug build
    config: Conditionally enable dev keys signing
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    
    config/version: PRODUCT_VERITY_SIGNING_KEY is deprecated
    
    avb built on older android with same key will still flash and run just
    fine locked on 14.
    
    TODO: consider dropping the deprecation of VB1.0 in build
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    
    signing: support inline signing for U [2/2]
    
    For those who have used .android-certs previously, cp .android-certs/* vendor/voltage/signing/keys/
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    
    version: Sign every build variant but eng
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    
    Co-authored-by: RealJohnGalt <RealJohnGalt@users.noreply.github.com>

--- config/version.mk
+++ config/version.mk
@@ -11,3 +11,14 @@ LINEAGE_VERSION := Blackiron-$(BLKI_VERSION)_$(LINEAGE_BUILD)-$(PRODUCT_VERSION_
 
 # Display version
 LINEAGE_DISPLAY_VERSION := Blackiron-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_BUILD)-v$(BLKI_VERSION)
+
+# Sign Build
+ifneq (eng,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard vendor/lineage/signing/keys/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage/signing/keys/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+ifneq (,$(wildcard vendor/lineage/signing/keys/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := vendor/lineage/signing/keys/otakey.x509.pem
+endif
+endif
