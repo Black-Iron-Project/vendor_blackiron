35561df01d5c4ed4089a60cd30193a735a60c9ce
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-17 12:58:23 +0800

    Introduce bootanimation styles [3/3]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,14 +1,8 @@
 
 # Bootanimation
-ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else
-    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
-        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
-    else
-        $(warning "Current bootanimation res is not supported, forcing 1080p")
-    endif
-    PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-endif
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilts/bootanimation/bootanimation_blackiron.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation_blackiron.zip \
+    vendor/lineage/prebuilts/bootanimation/bootanimation_cyberpunk.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation_cyberpunk.zip \
+    vendor/lineage/prebuilts/bootanimation/bootanimation_valorant.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation_valorant.zip \
+    vendor/lineage/prebuilts/bootanimation/bootanimation_google.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation_google.zip \
+    vendor/lineage/prebuilts/bootanimation/bootanimation_google_monet.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation_google_monet.zip
