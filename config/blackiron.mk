c7483f80078ff5d644473965b8749fd91fc562a3
ralf979 <ralf979@gmail.com>
2025-03-24 08:51:27 +0100

    blackiron: update Gapps
    Change-Id: I90ae26d3caed4965dde71888a8a80aa1375023e7

--- config/blackiron.mk
+++ config/blackiron.mk
@@ -1,9 +1,7 @@
 -include vendor/addons/config.mk
 WITH_GMS ?= false
 ifeq ($(WITH_GMS),true)
--include vendor/gapps/config.mk
-    VENDOR_EXCEPTION_PATHS += \
-    gapps
+-include vendor/gapps/arm64/arm64-vendor.mk
 endif
 include vendor/blackiron/config/properties.mk
 include vendor/blackiron/config/packages.mk
